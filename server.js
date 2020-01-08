5087887cafff39f98602e8a32d92efe65551b1d0
Jarod Peachey <jwpeachey107@aol.com>
2020-01-08 09:17:47 -0500

    Start on design

--- server.js
+++ server.js
@@ -1,19 +1,49 @@
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
+// Dependencies
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
 
-// Require Express to run server and routes
+// Empty data object
+projectData = {
+  entries: [
+
+  ],
+};
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+//Here we are configuring express to use body-parser as middle-ware, as well as cors
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static('src'));
 
+// Initialize server
+const port = 3000;
 
-// Setup Server
+const server = app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
+
+// Set up HTTP request routes
+app.get('/data', sendData);
+app.post('/data', postData);
+
+// Set up functions for HTTP requests
+function sendData (request, response) {
+  response.send(projectData);
+}
+
+function postData (request, response) {
+  const newObject = {
+    ...request.body,
+    id: projectData.entries[projectData.entries.length - 1].id + 1 || 1,
+  };
+
+  projectData.entries.push(newObject);
+  response.send('POST recieved');
+}
