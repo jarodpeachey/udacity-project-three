2d7dc6a0f1b4869f7c0186dd06a6d4e6323a7a10
Jarod Peachey <jwpeachey107@aol.com>
2020-01-08 22:55:23 -0500

    Add request to OWM

--- src/app.js
+++ src/app.js
@@ -1,5 +1,38 @@
 /* Global Variables */
+let addEntryButton = document.getElementById('entry-button');
+let name = document.getElementById('name');
+let zip = document.getElementById('zip');
+let feelings = document.getElementById('feelings');
+
+// Event Listener
+addEntryButton.addEventListener('click', addEntry);
+
+// Add Entry
+function addEntry (e) {
+  e.preventDefault();
+  if (name.value == '' || zip.value == '' || feelings.value == '') {
+    alert('Please fill in all the fields.');
+  } else {
+    getWeatherData(zip.value, '8e5ccdb71fc19dc417aad096b7f8d68c');
+  }
+};
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+async function getWeatherData(zipCode, apiKey) {
+  console.log("Getting weather data from zip code " + zipCode);
+
+  const response = await fetch(
+    `https://api.openweathermap.org/data/weather?zip=${zipCode}&APPID=${apiKey}`,
+  );
+
+  try {
+    const allData = await response.json();
+
+    console.log(allData);
+  } catch (error) {
+    console.log(error);
+  }
+}
